33e7bac3343afc42b3b5436e7b9b52b766834bb5
yoni-jo <yoni9258@naver.com>
2021-10-02 17:02:05 +0900

    🩹 JS: rooms메뉴를 눌러도  privaterooms 감쳐진 세션이 나오게 수정.

--- js/dropdown.js
+++ js/dropdown.js
@@ -21,15 +21,19 @@ etc_sns.addEventListener('mouseleave', () => {
 
 //private rooms btn클릭 이벤트 
 
-const private_btn = document.querySelector('.btn--private');
+const private_btn = document.querySelectorAll('.open_rooms');
 const private_rooms = document.querySelector('.private_rooms');
 const close_btn = document.querySelector('.close')
 const VIEW = 'view'
 
-private_btn.addEventListener('click', () => {
-    private_rooms.classList.add(VIEW)
-    close_btn.addEventListener('click', () => {
-        private_rooms.classList.remove(VIEW)
+private_btn.forEach((private_btn) => {
+    private_btn.addEventListener('click', () => {
+        console.log('ddd')
+        private_rooms.classList.add(VIEW)
+        close_btn.addEventListener('click', () => {
+            private_rooms.classList.remove(VIEW)
+        })
+    
     })
-
 })
+
