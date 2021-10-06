db1f22ce3aae2bcd0bd422f6376d265e79f115d9
yoni-jo <yoni9258@naver.com>
2021-10-06 18:30:01 +0900

    🔥 라이브러리 swiper => tiny-slider로 변경

--- js/slide.js
+++ js/slide.js
@@ -1,25 +1,20 @@
 
 //OFFER 세션
-const swiper = new Swiper('.offers', {
-    // Optional parameters
-    direction: 'horizontal',
-    slidesPerView: 2,
-    navigation: {
-        nextEl: '.offers .swiper-button-next',
-        prevEl: '.offers .swiper-button-prev',
-    },
-});
+const offers = tns({
+    container: '.offers .slider_container',
+    slideBy: 'page',
+    items:3,
+    mouseDrag: true,
+    controlsText:["<i class='fas fa-chevron-left'></i>","<i class='fas fa-chevron-right'></i>"],
+    controlsPosition:"bottom"
+  });
 
 
-//SLIDE SHOW 세션
-new Swiper('.slide_show', {
-    direction: 'horizontal',
-    slidesPerView:1,
-    spaceBetween:10,
-    loop: true,
-    centeredSlides: true,
-    navigation: {
-        prevEl: '.slide_show .swiper-button-prev',
-        nextEl: '.slide_show .swiper-button-next'
-    }
-});
+//SLIDE SHOW 세션 
+const slide_show = tns({
+      container: '.slide_show .slider_container',
+      slideBy: 'page',
+      mouseDrag: true,
+      controlsText:["<i class='fas fa-chevron-left'></i>","<i class='fas fa-chevron-right'></i>"],
+      controlsPosition:"bottom"
+    });
